5160d9fa7aebe9598c96c6105d29191806bac285
unknown-salt <217187386+unknown-salt@users.noreply.github.com>
2026-06-10 23:57:38 +0200

    Initial 26.1 migration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,15 @@
 pluginManagement {
-	repositories {
-		maven {
-			name = "Fabric"
-			url = uri("https://maven.fabricmc.net/")
-		}
-		mavenCentral()
-		gradlePluginPortal()
-	}
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
 
-	plugins {
-		id("net.fabricmc.fabric-loom-remap") version providers.gradleProperty("loom_version")
-	}
+    plugins {
+        id("net.fabricmc.fabric-loom") version providers.gradleProperty("loom_version")
+        kotlin("jvm") version providers.gradleProperty("kotlin_version")
+    }
 }
