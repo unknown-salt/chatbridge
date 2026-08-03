056a38081e140284662cb7e067c97231a52f517d
unknown-salt <217187386+unknown-salt@users.noreply.github.com>
2026-08-03 11:43:30 +0200

    fix: update screen handling

--- src/main/java/com/chatbridge/utils/ChatBridgeCommands.kt
+++ src/main/java/com/chatbridge/utils/ChatBridgeCommands.kt
@@ -13,7 +13,7 @@ object ChatBridgeCommands {
                     val client = Minecraft.getInstance()
                     val screen = ChatBridgeConfigManager.build(null)
                     client.schedule {
-                        client.setScreen(screen)
+                        client.gui.setScreen()
                     }
                     1
                 }
